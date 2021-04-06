1dee98f60be81eeb5204d165d49565bc61d4cd70
Joel Eckerson <joelaeckerson@gmail.com>
2021-04-06 11:48:02 -0500

    question 2

---
+++ joins-solution.sql
@@ -0,0 +1,26 @@
+1. Get all customers and their addresses.
+SELECT * FROM customers
+JOIN addresses ON addresses.customer_id = customers.id;
+
+2. Get all orders and their line items (orders, quantity and product).
+SELECT *
+FROM orders
+JOIN line_items ON orders.id = line_items.order_id;
+
+3. Which warehouses have cheetos?
+
+
+4. Which warehouses have diet pepsi?
+
+
+5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+
+
+6. How many customers do we have?
+
+
+7. How many products do we carry?
+
+
+8. What is the total available on-hand quantity of diet pepsi?
+
